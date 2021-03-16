1a79def8fadc7b4405a872d98df04a3d5e5949bd
Hridoy <hridoykrisnadas@gmail.com>
2021-03-16 19:01:53 +0600

    master

--- app/src/main/java/com/shitsolution/mynotes/MainActivity.java
+++ app/src/main/java/com/shitsolution/mynotes/MainActivity.java
@@ -1,14 +1,27 @@
 package com.shitsolution.mynotes;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    ImageView quickAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        quickAdd = findViewById(R.id.quickAddId);
+        quickAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CreateNotesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
