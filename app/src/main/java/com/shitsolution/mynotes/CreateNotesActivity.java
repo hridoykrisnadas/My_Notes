532e7a7aa3a1bff283e4e6a1eb396341f37557bb
Hridoy <hridoykrisnadas@gmail.com>
2021-03-16 19:17:19 +0600

    master

--- app/src/main/java/com/shitsolution/mynotes/CreateNotesActivity.java
+++ app/src/main/java/com/shitsolution/mynotes/CreateNotesActivity.java
@@ -1,21 +1,17 @@
 package com.shitsolution.mynotes;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateNotesActivity extends AppCompatActivity {
 
-    View subtitleIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_notes);
 
-        subtitleIndicator = findViewById(R.id.subtitleIndicatorID);
-        subtitleIndicator.setBackgroundColor(Color.parseColor("#FABE2B"));
+
     }
 }
